c78ee2e8f5480146d8c067f19e46e6e741abb917
Mariano Guerra <mariano@marianoguerra.org>
2019-12-02 16:35:09 +0100

    html and mkdocs builds

---
+++ mkdocs/diameter/depend.mk
@@ -0,0 +1,6 @@
+$(HTMLDIR)/diameterc.html: diameterc.xml
+$(HTMLDIR)/diameter_intro.html: diameter_intro.xml
+$(HTMLDIR)/diameter_using.html: diameter_using.xml
+$(HTMLDIR)/diameter_examples.html: diameter_examples.xml
+$(HTMLDIR)/diameter_soc.html: diameter_soc.xml
+$(HTMLDIR)/notes.html: notes.xml
